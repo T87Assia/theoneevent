c01e524adbd6a610bfbc0c6ab4632430addd4867
boulaffasae <a.boulaffas@fullwaveagency.com>
2019-06-17 19:03:54 +0100

    DEV: Set admin password to `admin`

--- weddingorganizer.sql
+++ weddingorganizer.sql
@@ -265,7 +265,7 @@ LOCK TABLES `users` WRITE;
 
 INSERT INTO `users` (`user_id`, `name`, `username`, `no_telp`, `password`)
 VALUES
-	(1,'admin','admin','','admin');
+	(1,'admin','admin','','21232f297a57a5a743894a0e4a801fc3');
 
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
